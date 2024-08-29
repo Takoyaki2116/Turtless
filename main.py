212793b81bd1359ed863661f5622cad12b21580d
Takoyaki2116 <sasakisora2116@gmail.com>
2024-08-29 10:51:30 +0000

    新しく直した

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("top.html")
+
+app.run(host="0.0.0.0", port=8080)
