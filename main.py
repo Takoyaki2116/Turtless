c24e0eb7870189cb0f54d5d58efea242d3d5458d
Takoyaki2116 <sasakisora2116@gmail.com>
2024-08-29 11:04:30 +0000

    色々追加した

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    return redirect("/list")
+
+@app.route("/list")
+def list():
     return render_template("top.html")
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/edit")
+def edit():
+    return render_template("edit.html")
 
 app.run(host="0.0.0.0", port=8080)
